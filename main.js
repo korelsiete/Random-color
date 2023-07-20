3dbd8f46c917c8dd9d9be7a1acd7edfb7dc0b70e
korelss7 <pajita200305@gmail.com>
2023-07-20 15:50:51 -0500

    changed the button event and added the output of the hexadecimal code of the random color

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const cuadro = document.querySelector("#randomColor");
+const fondo = document.querySelector("#bgRandom");
 const btnChange = document.querySelector("#btnChange");
+const newHex = document.querySelector("#newHex");
 
 btnChange.addEventListener("click", () => {
-  cuadro.setAttribute("style", `background-color: ${randomColorHex()}`);
+  const randomHex = randomColorHex();
+  fondo.setAttribute("style", `background-color: ${randomHex}`);
+  newHex.textContent = randomHex;
 });
 
 function randomColorHex() {
-  const conjunto = "0123456789abcdef";
+  const conjunto = "0123456789ABCDEF";
   let random = "#";
   for (let i = 0; i < 6; i++) {
     random += conjunto[Math.floor(Math.random() * conjunto.length)];
